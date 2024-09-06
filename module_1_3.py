749aca37627b62173bfc88b7d8d7aaef5259d0fc
Alex Mad <64276552+n13cwg@users.noreply.github.com>
2024-09-06 11:24:53 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Alexandra'
+print('Name: ' + str(name))
+
+age = 40
+print('Age: ' + str(age))
+
+age = age + 1
+print('New age: ' + str(age))
+
+is_student = True
+print('Is Student: ' + str(is_student))
