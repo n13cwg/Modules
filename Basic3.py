9fd49f76698e6e35f7ea87298ee0f08ae9cc0e6e
Alex Mad <64276552+n13cwg@users.noreply.github.com>
2024-09-06 10:17:45 +0300

    Add files via upload

---
+++ Basic3.py
@@ -0,0 +1,6 @@
+tasks_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / tasks_done
+print(('Курс: ' + course_name + ', ' + 'всего задач: ' + str(tasks_done) + ', ' + 'затрачено часов: ' + str(
+    hours_spent) + ', ' + 'среднее время выполнения задания ' + str(time_for_one_task)))
