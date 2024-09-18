876f5784a44a11dec31788e4d254c90ba91bfe0b
Alex Mad <64276552+n13cwg@users.noreply.github.com>
2024-09-18 14:45:55 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -24,8 +24,8 @@ def is_contains(string, list_to_search):
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
 
 
