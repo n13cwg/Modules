0cf52907d439df0ad298f884491743d72ce402fc
Alex Mad <64276552+n13cwg@users.noreply.github.com>
2024-09-18 08:44:28 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,14 @@
-def find_password(n): # n - число, отображаемое в первой вставке ворот
-    result = "" # пустая строка для хранения пароля
-    for i in range(1, 21): # перебрать все возможные пары чисел i и j от 1 до 20
-        for j in range(1, 21):
-            if (i + j) % n == 0 and i != n and j != n:
-                result = str(i) + str(j) # сохраняет пароль
-                return result
-    return "Пароль не найден"
+def generate_password(n):
+    result = "" # Для хранения пароля
+    for i in range(1, n): # Цикл перебирает числа от 1 до n-1
+        for j in range(i+1, n+1): # Цикл перебирает числа от i+1 до n
+            if (i + j) == n: # Проверка, кратно ли n сумме i и j
+                result += str(i) + str(j) # Добавление пары чисел в строку result
+    return result
 
-n = int(input('Введите число: '))
-password = find_password(n)
-print(f'Пароль для числа {n}: {password}')
+n = int(input("Введите число от 3 до 20: "))
+if 3 <= n <= 20:
+    password = generate_password(n)
+    print(f"Пароль для числа {n}: {password}")
+else:
+    print("Неверный ввод числа. Число должно быть от 3 до 20.")
