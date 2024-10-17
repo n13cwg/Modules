503e59bbc9ebd31d5de156dc88d7373a669f2646
Alex Mad <64276552+n13cwg@users.noreply.github.com>
2024-10-17 15:46:06 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+# Вызов inner_function вне test_function выдаст ошибку NameError: name 'inner_function' is not defined
+
+test_function() # Вызов test_function, который, вызовет inner_function
